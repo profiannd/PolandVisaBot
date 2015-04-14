a725e44be012b13d36b91f02a0123e1d557e05f1
icher78 <icher78@inbox.ru>
2015-04-14 23:51:16 +0300

    dupl form

---
+++ PolandVisaAuto/DuplForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PolandVisaAuto
+{
+    public partial class DuplForm : Form
+    {
+        public DuplForm()
+        {
+            InitializeComponent();
+        }
+
+        public void FillCombo(List<string> dsource)
+        {
+            comboBox1.DataSource = dsource;
+        }
+
+        public string GetSelectedCity()
+        {
+            return comboBox1.SelectedItem.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
